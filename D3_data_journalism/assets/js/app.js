9c1cc19c33fbe281c88b8e97ebdef8a120905748
John Cass <cass.john@icloud.com>
2021-07-29 13:41:55 -0500

    updated comments

--- D3_data_journalism/assets/js/app.js
+++ D3_data_journalism/assets/js/app.js
@@ -1,4 +1,4 @@
-// I worked with Erin Wills (TA) and the following people:  ______________
+// I worked with Erin Wills (TA) 
 // Remember to comment your code as we discuss it.  Commenting the code is required.
 // Prior to starting, make sure  you have already created and cloned your repo.
 
